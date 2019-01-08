d420d5b0a376ca04c238821b40e0688eb0f11956
Victor Shahbazian <victor.sh.91@gmail.com>
2019-01-07 23:45:38 -0800

    changed googled analytics got use ga.js plugin instead of nuxt package

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '~plugins/ga.js', ssr: false }
   ],
 
   /*
@@ -42,10 +43,7 @@ module.exports = {
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
     // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt',
-    '@nuxtjs/google-analytics', {
-      id: 'UA-36377247-2'
-    }
+    'bootstrap-vue/nuxt'
   ],
   /*
   ** Axios module configuration
