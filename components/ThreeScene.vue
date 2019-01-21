69ed30fb17ebd08e28e267694ed9477e0c85348a
Victor Shahbazian <victor.sh.91@gmail.com>
2019-01-21 15:05:07 -0800

    updated loading for three

--- components/ThreeScene.vue
+++ components/ThreeScene.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import * as Three from 'three'
+import * as THREE from 'three'
 import OrbitControls from 'orbit-controls-es6'
 import { mapState, mapActions, mapMutations } from 'vuex'
 
@@ -35,84 +35,75 @@ export default {
   created: function(){
   },
   mounted() {
-    this.init();
-    this.animate();
+    this.init().then(()=>{
+      this.animate();
+    });
   },
   destroyed: function() {
     document.removeEventListener('resize', this.onWindowResize, false);
   },
   methods: {
-    init: function() {
+    loadTextures: async function() {
+
+    },
+    init: async function() {
       let container = document.getElementById('three-container');
       let width = window.innerWidth;
       let height = window.innerHeight;
-      this.camera = new Three.PerspectiveCamera(75, width/height, 0.1, 1000);
-      this.clock = new Three.Clock();
+      this.camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
+      this.clock = new THREE.Clock();
 
-      this.renderer = new Three.WebGLRenderer({antialias: true});
+      this.renderer = new THREE.WebGLRenderer({antialias: true});
       this.renderer.setClearColor('#000000')
       this.renderer.setSize(width, height);
       this.camera.position.z = 10;
       this.controls = new OrbitControls(this.camera, this.renderer.domElement);
       this.controls.update()
 
-      this.scene = new Three.Scene();
+      this.scene = new THREE.Scene();
 
       // Loading
-      this.loadingManagerThree = new Three.LoadingManager(() => {
-        console.log('loading');
-      });
-      this.loader = new Three.ImageLoader();
-      this.loader.load(
-        '/particle.png',
-        function (image) {
-          console.log('loaded');
-        },
-        undefined,
-        function () {
-          console.error('An error has occurred loading the particle image');
-        }
-      )
+      let particleTexture = await this.loadTexture('/particle_white.png');
 
-      this.light = new Three.DirectionalLight(0xFFFFFF);
+      this.light = new THREE.DirectionalLight(0xFFFFFF);
       this.scene.add(this.light);
 
-      this.pointsGeometry = new Three.Geometry()
+      this.pointsGeometry = new THREE.Geometry();
       for(let i = 0; i<1000; i++){
-        var point = new Three.Vector3()
-        point.x = Three.Math.randFloatSpread(100)
-        point.y = Three.Math.randFloatSpread(100)
-        point.z = Three.Math.randFloatSpread(100)
+        var point = new THREE.Vector3()
+        point.x = THREE.Math.randFloatSpread(100)
+        point.y = THREE.Math.randFloatSpread(100)
+        point.z = THREE.Math.randFloatSpread(100)
         this.pointsGeometry.vertices.push(point)
       }
-      this.pointsMaterial = new Three.PointsMaterial({
-        color: 0xFFFFFF, map: Three.ImageUtils.loadTexture(
-          "/particle_white.png"
-        ),
-        blending: Three.AdditiveBlending,
+      // let particleTexture = new THREE.TextureLoader().load('/particle_white.png');
+      this.pointsMaterial = new THREE.PointsMaterial({
+        color: 0xFFFFFF, map: particleTexture,
+        blending: THREE.AdditiveBlending,
         transparent: true
       })
-      this.pointsSystem = new Three.Points(this.pointsGeometry, this.pointsMaterial)
+      this.pointsSystem = new THREE.Points(this.pointsGeometry, this.pointsMaterial);
       this.pointsSystem.sortParticles = true;
-      this.scene.add(this.pointsSystem)
+      this.scene.add(this.pointsSystem);
 
-      // let geometry = new Three.BoxGeometry(2, 2, 2);
+      // let geometry = new THREE.BoxGeometry(2, 2, 2);
       let geometry;
       let material;
       if(this.$route.path === '/') {
-        geometry = new Three.OctahedronBufferGeometry(2, 0);
-        material = new Three.MeshPhongMaterial({color: '#00d0ff'});
+        geometry = new THREE.OctahedronBufferGeometry(2, 0);
+        material = new THREE.MeshPhongMaterial({color: '#00d0ff'});
       } else if (this.$route.path === '/about') {
-        geometry = new Three.IcosahedronBufferGeometry(2, 0);
-        material = new Three.MeshPhongMaterial({color: '#1eff8b', wireframe: true});
+        geometry = new THREE.IcosahedronBufferGeometry(2, 0);
+        material = new THREE.MeshPhongMaterial({color: '#1eff8b', wireframe: true});
       }
       
-      this.mesh = new Three.Mesh(geometry, material);
+      this.mesh = new THREE.Mesh(geometry, material);
       this.scene.add(this.mesh);
 
       container.appendChild(this.renderer.domElement);
       window.addEventListener('resize', this.onWindowResize, false)
 
+      await timeout(3000);
     },
     animate: function() {
       requestAnimationFrame(this.animate);
@@ -147,6 +138,11 @@ export default {
       this.controls.update();
       this.renderer.render(this.scene, this.camera);
     },
+    loadTexture: function(url) {
+      return new Promise(resolve => {
+        new THREE.TextureLoader().load(url, resolve);
+      })
+    },
     onWindowResize: function () {
       let width = this.getWindowWidth();
       let height = this.getWindowHeight();
@@ -162,6 +158,10 @@ export default {
       return window.innerHeight;
     }
   }
+}
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 </script>
 
